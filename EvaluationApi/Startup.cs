42405297680b25645659e96ef77c570f4ece229f
Nathan <l.nathan97460@gmail.com>
2022-06-03 10:48:46 +0200

    fix config

--- EvaluationApi/Startup.cs
+++ EvaluationApi/Startup.cs
@@ -1,6 +1,4 @@
 using EvaluationApi.Models;
-using EvaluationApi.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,11 +8,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace EvaluationApi
@@ -37,7 +33,6 @@ namespace EvaluationApi
                 c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "POI-App", Version = "v1" });
             });
 
-            // Service de connexion à la bdd 
             var connectionString = _configuration.GetConnectionString("Default");
 
             var serverVersion = new MariaDbServerVersion(new Version(10, 1, 45));
@@ -46,29 +41,7 @@ namespace EvaluationApi
                DbContextOptions => DbContextOptions
                     .UseMySql(connectionString, serverVersion));
 
-            // Service d'authentification 
-            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddJwtBearer(options =>
-                {
-                    options.TokenValidationParameters = new TokenValidationParameters
-                    {
-                        ValidateIssuer = true,
-                        ValidateAudience = true,
-                        ValidateLifetime = true,
-                        ValidateIssuerSigningKey = true,
-                        ValidIssuer = Configuration["Jwt:Issuer"],
-                        ValidAudience = Configuration["Jwt:Audience"],
-                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
-                    };
-                });
-
-            services.AddTransient<ILoginService, LoginService>();
-
-            services.AddDbContext<PointOfInterestContext>(opt =>
-                opt.UseInMemoryDatabase("POI-Liste"));
-
             services.AddControllers();
-
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -90,7 +63,7 @@ namespace EvaluationApi
             app.UseHttpsRedirection();
 
             app.UseRouting();
-            app.UseAuthentication();
+
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
