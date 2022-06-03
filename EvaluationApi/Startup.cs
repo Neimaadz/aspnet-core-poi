9a1936ecca081d4072032b23d6cd39b9de083951
Nathan <l.nathan97460@gmail.com>
2022-06-03 11:16:00 +0200

    fix merge

--- EvaluationApi/Startup.cs
+++ EvaluationApi/Startup.cs
@@ -1,4 +1,6 @@
 using EvaluationApi.Models;
+using EvaluationApi.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -8,19 +10,19 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Text;
 
 namespace EvaluationApi
 {
     public class Startup
     {
-        public IConfiguration _configuration { get; set; }
         public Startup(IConfiguration configuration)
         {
-            _configuration = configuration;
+            Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
@@ -33,13 +35,35 @@ namespace EvaluationApi
                 c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "POI-App", Version = "v1" });
             });
 
-            var connectionString = _configuration.GetConnectionString("Default");
+            // Service de connexion a la bdd
+            var connectionString = Configuration.GetConnectionString("Default");
 
             var serverVersion = new MariaDbServerVersion(new Version(10, 1, 45));
 
             services.AddDbContext<PointOfInterestContext>(
                DbContextOptions => DbContextOptions
                     .UseMySql(connectionString, serverVersion));
+
+            // Service d'authentification 
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = Configuration["Jwt:Issuer"],
+                        ValidAudience = Configuration["Jwt:Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
+                    };
+                });
+
+            services.AddTransient<ILoginService, LoginService>();
+
+            services.AddDbContext<PointOfInterestContext>(opt =>
+                opt.UseInMemoryDatabase("POI-Liste"));
 
             services.AddControllers();
         }
@@ -63,6 +87,8 @@ namespace EvaluationApi
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
