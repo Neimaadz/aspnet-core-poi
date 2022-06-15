9f42713c31320e8d7e27c0ba19237c658b14a4d3
Nathan <l.nathan97460@gmail.com>
2022-06-15 22:26:51 +0200

    Fix précedent commit

--- EvaluationApi/Models/PointOfInterestItem.cs
+++ EvaluationApi/Models/PointOfInterestItem.cs
@@ -13,5 +13,25 @@ namespace EvaluationApi.Models
         public string Comment { get; set; }
         public double Lat { get; set; }
         public double Lng { get; set; }
+
+        public PointOfInterestItem(long Id, string Name, string ImagePath, string Comment, double Lat, double Lng)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.ImagePath = ImagePath;
+            this.Comment = Comment;
+            this.Lat = Lat;
+            this.Lng = Lng;
+        }
+        public PointOfInterestItem(string Name, string ImagePath, string Comment, double Lat, double Lng)
+        {
+            this.Name = Name;
+            this.ImagePath = ImagePath;
+            this.Comment = Comment;
+            this.Lat = Lat;
+            this.Lng = Lng;
+        }
+
     }
+
 }
