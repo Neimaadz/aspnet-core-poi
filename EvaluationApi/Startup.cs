3ec819e57042f843ba1f6e7df376083c5628df4d
Neimaad <damienlaoussing@hotmail.com>
2022-06-17 21:45:41 +0200

    udpate

--- EvaluationApi/Startup.cs
+++ EvaluationApi/Startup.cs
@@ -32,14 +32,6 @@ namespace EvaluationApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            var environmentVar = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT ");
-            if (environmentVar == "Production")
-            {
-                var prodCurrentWorkingDirectory = Configuration["ProdCurrentWorkingDirectory"];
-                Directory.SetCurrentDirectory(prodCurrentWorkingDirectory);
-                if (!Directory.Exists(prodCurrentWorkingDirectory + "/Ressources")) Directory.CreateDirectory(prodCurrentWorkingDirectory + "/Ressources");
-            }
-
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "POI-App", Version = "v1" });
@@ -80,6 +72,13 @@ namespace EvaluationApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            var environmentVar = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            if (environmentVar == "Production")
+            {
+                var prodCurrentWorkingDirectory = Configuration["ProdCurrentWorkingDirectory"];
+                Directory.SetCurrentDirectory(prodCurrentWorkingDirectory);
+                if (!Directory.Exists(prodCurrentWorkingDirectory + "/Ressources")) Directory.CreateDirectory(prodCurrentWorkingDirectory + "/Ressources");
+            }
             app.UseSwagger();
 
             app.UseSwaggerUI(c =>
